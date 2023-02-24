46e5cdb6da0e45fc80dbfef498dadc820f1d5e9d
Juan G. Molina <jg.molina92@hotmail.com>
2023-02-23 19:37:43 -0500

    Modify pokemon type color handling

--- components/badge/Badge.tsx
+++ components/badge/Badge.tsx
@@ -1,9 +1,11 @@
 import { PokemonType } from '@/types';
+
 import style from './Badge.module.scss';
+import typeColors from '../../styles/typeColors.module.scss';
 
 export function Badge({ type }: { type: PokemonType }) {
   return (
-    <span className={`${style.badge__content} ${style['type__' + type]}`}>
+    <span className={`${style.badge__content} ${typeColors['type__' + type]}`}>
       {type.charAt(0).toUpperCase() + type.slice(1)}
     </span>
   );
