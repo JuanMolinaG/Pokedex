8e9e6d396755c1ac8ad31bd33a97e4aee1800630
Juan G. Molina <jg.molina92@hotmail.com>
2023-02-24 21:34:23 -0500

    Install and configure @svgr/webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -12,5 +14,20 @@ module.exports = {
   },
   features: {
     interactionsDebugger: true
+  },
+  webpackFinal: async config => {
+    // this modifies the existing image rule to exclude .svg files
+    // since we want to handle those files with @svgr/webpack
+    const imageRule = config.module.rules.find(rule => rule.test.test('.svg'))
+    imageRule.exclude = /\.svg$/
+
+    // configure .svg files to be loaded with @svgr/webpack
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
   }
 }
